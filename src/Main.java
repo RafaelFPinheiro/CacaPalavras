a1549f64980b29260c72ed2a57b3dbd9fa5cdd0c
Rafael F Pinheiro <Rafael F Pinheiro@DESKTOP-MG4TQDO>
2018-07-03 20:26:14 -0300

    Finalizado busca das palavras na horizontal.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
@@ -13,7 +16,7 @@ public class Main {
 		
 		try {
 			String result = instance.doOCR(imageFile);
-			getFromDictionary(result);
+			setMatrizWords(result);
 //			System.out.println(result.contains("PINTOR"));
 		} catch (TesseractException e) {
 			System.err.println(e.getMessage());
@@ -21,8 +24,29 @@ public class Main {
 
 	}
 	
-	public static void getFromDictionary(String result) {
-		System.out.println(result.split("\n"));
+	public static void setMatrizWords(String result) {
+		int size = result.split("\n").length;
+		
+		for(int i=0;i<size;i++) {
+			getFromDictionary(result.split("\n")[i], i+1);
+		}
+	}
+	
+	public static void getFromDictionary(String line, int position) {
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader("dic.txt"));
+			while(br.ready()) {
+				String wordFromDictionary = br.readLine();
+				if(line.toLowerCase().contains(wordFromDictionary) && wordFromDictionary.length() > 3) {
+					System.out.println("Palavra encontrada: "+wordFromDictionary+", linha:"+ position);
+				}
+		      }
+		      br.close();
+		    } catch (IOException e) {
+		        System.err.printf("Erro na abertura do arquivo: %s.\n",
+		         e.getMessage());
+		    }
 		
 	}
 
